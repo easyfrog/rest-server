d3feb4e67d7a41b941164e714bb6332f0ee2fea7
easyfrog@126.com <easyfrog@126.com>
2022-09-15 00:52:40 +0800

    config add mongoAtlas url

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 module.exports = {
 	port: 3002,
+
+	// mongodb Atlas
 	// mongo_url: 'mongodb+srv://ztc:ztc888@cluster0.as8vaor.mongodb.net/base?retryWrites=true&w=majority',
+
+	// mongo docker image
 	mongo_url: 'mongodb://mongo:27017/base',
+	
 	db_name: 'base'
 }
